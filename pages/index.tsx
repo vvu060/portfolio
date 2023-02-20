a30822b104c5afdc24be0db4be8a004553aeda0d
vvu060 <vvu060@gmail.com>
2023-02-20 14:58:58 +0530

    add colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
+import { Inter, Montserrat } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,7 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Portfolio website</main>
+      <main className="h-screen w-screen bg-gray-700">
+        <h1 className="text-3xl text-purple-700">Portfolio website</h1>
+      </main>
     </>
   );
 }
